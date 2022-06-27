068b62eb66399f5c47797dcf92ef1c1a1013707a
Mário Menezes <mario082@gmail.com>
2022-06-27 20:37:35 -0300

    style and router

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
-import { gql, useQuery } from "@apollo/client"
+import { ApolloProvider, gql, useQuery } from "@apollo/client"
+import { BrowserRouter } from "react-router-dom"
+import { client } from "./lib/apollo"
 import {Event} from "./pages/Event"
+import {Router} from "./Router"
 
 
 function App() {
 
   return (
-    <div>
-      <Event />
-    </div>
+    <ApolloProvider client={client}>
+        <BrowserRouter>
+            <Router />
+        </BrowserRouter>
+    </ApolloProvider>
   )
 }
 
